ca8b472c9e9d36e146c2999127506f9d52f95ae4
ViciousPotato <Zhou.Honglin@Gmail.com>
2015-04-01 22:08:27 +0800

    Change whole UI

---
+++ Acan/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.Windows
+open FsXaml
+
+type window = XAML<"MainWindow.xaml">
+
+[<STAThread()>]
+[<EntryPoint>]
+let main argv =
+  let w = window()
+
+  w.Root.MouseDown.Add (fun _ -> w.Root.DragMove())
+
+  w.Root.Show()
+
+  let app = Application()
+  app.Run()
--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem "tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\FAKE\tools\Fake.exe" build.fsx
